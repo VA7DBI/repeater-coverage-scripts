a50fad4ad7f8d021f0f542e191071a7d780ab99b
VA7DBI - DarcyB <DarcyB+va7dbi@gmail.com>
2021-11-22 14:53:48 -0800

    Cleanup and extend the example to show better and more useful results

--- sql/examples.sql
+++ sql/examples.sql
@@ -1,12 +1,22 @@
 -- find the best repeter for a given point.
 
 WITH pt AS (
-  SELECT ST_SetSRID(ST_MakePoint(-119.3934, 49.9024), 4326)
-  AS pt )
-SELECT 255-ST_Value(rast, 5, pt.pt) as pathloss, t.callsign
-FROM transmitters t, coverages c
+  SELECT ST_SetSRID(ST_MakePoint(-119.3934169, 49.9024461), 4326) AS pt
+)
+SELECT 
+  t.callsign, 
+  b.notes AS Band,
+  (ST_DistanceSphere(pt,t.geom) || ' m')::unit @ 'km' AS Distance,
+  ST_Azimuth(pt.pt,t.geom)/(2*pi())*360  AS Heading,
+  255-ST_Value(rast, 5, pt.pt) AS pathloss
+FROM
+  band b, 
+  transmitters t, 
+  coverages c
 CROSS JOIN pt
 WHERE ST_Intersects(pt.pt, st_convexhull(c.rast)) 
+ AND b.id=t.band
  AND t.id=c.filename::int
- ORDER BY 1 ASC;
+ AND (255-ST_Value(rast, 5, pt.pt)) < 200
+ORDER BY 5 ASC;
 
