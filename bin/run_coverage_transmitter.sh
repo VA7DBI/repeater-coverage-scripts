e3a12d6eb5906d60e76ad2374306e34f288bdb29
VA7DBI - DarcyB <DarcyB+va7dbi@gmail.com>
2022-01-08 06:53:27 -0800

    Updte coverage runners to prioritize transmitters which are more local

--- bin/run_coverage_transmitter.sh
+++ bin/run_coverage_transmitter.sh
@@ -1,31 +1,45 @@
 #!/bin/sh
 
-TRANSMITTERS=`psql -h pg12-01 -d cov2 -t -c "SELECT t.id, t.callsign, (b.low + (b.high - b.low)) @ 'MHz', (ST_AsLatLonText(t.geom, 'D M S')), t.band FROM transmitters t, band b WHERE b.id=t.band AND t.callsign = '$1' LIMIT 1"`
+
+TRANSMITTERS=`psql -h pg12-01 -d cov2 -t -c "SELECT t.id, t.callsign, (b.low + (b.high - b.low)) @ 'MHz', (ST_x(t.geom) || ' ' || ST_y(t.geom)), t.band,  round(extract(epoch from t.lastupdate)) FROM transmitters t, band b WHERE b.id=t.band AND t.id = '$1' LIMIT 1"`
 FILENAME=`echo $TRANSMITTERS  | awk -F\| '{print $2}' | awk '{$1=$1};1'`
 FREQUENCY=`echo $TRANSMITTERS | awk -F\| '{print $3}' | awk '{print $1}'`
 LOCATION=`echo $TRANSMITTERS  | awk -F\| '{print $4}'`
 BAND=`echo $TRANSMITTERS  | awk -F\| '{print $5}' | awk '{$1=$1};1'`
 TXID=`echo $TRANSMITTERS | awk -F\| '{print $1}' | awk '{$1=$1};1'`
-LAT=`echo $LOCATION | awk '{print ($1 " " $2 " " $3)}'`
-LONG=`echo $LOCATION | awk '{print ($4 " " $5 " " $6)}'`
+LASTMOD=`echo $TRANSMITTERS | awk -F\| '{print $6}' | awk '{$1=$1};1'`
+LAT=`echo $LOCATION | awk '{print ($2)}'`
+LONG=`echo $LOCATION | awk '{print ($1)}'`
 LONGFIX=`echo ${LONG} | tr -d \-`
-ELEVATION="4 Meters"
+TXELEVATION="4 Meters"
+#os specifics
+OS=`uname`
+case ${OS} in
+  Linux)
+    OS_STAT="stat -c %Y";;
+  FreeBSD)
+    OS_STAT="stat -c %Y";;
+esac
+
 mkdir -p processing/${BAND}
 rm processing/${BAND}/${TXID}.qth
 echo "${FILENAME}" > processing/${BAND}/${TXID}.qth
 echo "${LAT}" >> processing/${BAND}/${TXID}.qth
 echo "${LONGFIX}" >> processing/${BAND}/${TXID}.qth
-echo "${ELEVATION}" >> processing/${BAND}/${TXID}.qth
-if [ -f "processing/${BAND}/${TXID}_ploss.tif" ] ; then
+echo "${TXELEVATION}" >> processing/${BAND}/${TXID}.qth
+STAT=`${OS_STAT} "processing/${BAND}/${TXID}_ploss.tif"`
+echo "$LOCATION stat ${STAT} lastmod ${LASTMOD} : ${LAT} , ${LONG} (${LONGFIX})"
+if [ "${DEBUG}NO" != "NO"  -o -f "processing/${BAND}/${TXID}_ploss.tif" -a ${STAT} -ge ${LASTMOD} ] ; then
   echo "Skipping ${FILENAME}"
 else
-  splat -d /usr/local/data2/mapping/sdf/ -f ${FREQUENCY} -L 1.0  -v 255 -gc -R 50 -hd -tif -ngs -sc -dbm -log -db -112 -erp 30 -t processing/${BAND}/${TXID} -o processing/${BAND}/${TXID}
-  gdal_translate -b 5 -a_nodata None -co TILED=YES -co COMPRESS=DEFLATE -co BIGTIFF=YES -of COG processing/${BAND}/${TXID}.tif processing/${BAND}/${TXID}_ploss.tif
+  splat -d /usr/local/data2/mapping/sdf/ -f ${FREQUENCY} -L 1.0  -v 255 -gc -hd -tif -ngs -sc -dbm -log -db -254 -erp 50 -t processing/${BAND}/${TXID} -o processing/${BAND}/${TXID}
+  mv "${FILENAME}-site_report.txt" processing/${BAND}/
+  gdal_translate -b 5 -a_nodata None -co COMPRESS=DEFLATE -co BIGTIFF=YES -of COG processing/${BAND}/${TXID}.tif processing/${BAND}/${TXID}_ploss.tif
   gdaladdo -r average processing/${BAND}/${TXID}_ploss.tif 2 4 8 16
   rm processing/${BAND}/${TXID}*.psql.gz
   raster2pgsql -s 4326 -t 256x256 -a -F processing/${BAND}/${TXID}_ploss.tif coverages > processing/${BAND}/${TXID}_ploss.psql
   psql -h pg12-01 -d cov2 -c "DELETE FROM coverages WHERE filename='${TXID}.tif' OR filename='${TXID}' OR filename='${TXID}_ploss.tiff';"
-  psql -h pg12-01 -d cov2 < processing/${BAND}/${TXID}_ploss.psql
-  psql -h pg12-01 -d cov2 -c "UPDATE coverages SET filename='${TXID}' WHERE filename='${TXID}_ploss.tif';"
+  psql -h pg12-01 -d cov2 < processing/${BAND}/${TXID}_ploss.psql >/dev/null
+  psql -h pg12-01 -d cov2 -c "UPDATE coverages SET filename='${TXID}' WHERE filename='${TXID}_ploss.tif';" 
   gzip processing/${BAND}/${TXID}*.psql
 fi
--- bin/runall.sh
+++ bin/runall.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for i in `psql -h pg12-01 cov2 -t -c "SELECT distinct callsign FROM transmitters WHERE length(callsign) = 6"`; do bin/run_coverage_transmitter.sh $i; done
+for i in `psql -h pg12-01 cov2 -t -c "SELECT id FROM transmitters ORDER BY st_distance(geom,st_setsrid(st_makepoint(-119.5588,49.9474),4326));"`; do bin/run_coverage_transmitter.sh $i; done
